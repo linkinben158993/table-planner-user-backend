7fe2339ddb2146fd1d29bfb10a522fd51eddc603
Linkin Ben <linkinben@localhost.localdomain>
2021-03-06 18:23:28 +0700

    Mocha Exit Flag Update

--- test/test.js
+++ test/test.js
@@ -1,28 +1,38 @@
-const { expect } = require('chai');
-const sum = require('./test.module');
+const chai = require('chai');
+const request = require('chai-http');
+const { server } = require('../app');
 
-describe('#sum()', () => {
-  context('without arguments', () => {
-    it('should return 0', () => {
-      expect(sum()).to.equal(0);
-    });
-  });
+chai.should();
+chai.use(request);
 
-  context('with number arguments', () => {
-    it('should return sum of arguments', () => {
-      expect(sum(1, 2, 3, 4, 5)).to.equal(15);
-    });
-
-    it('should return argument when only one argument is passed', () => {
-      expect(sum(5)).to.equal(5);
-    });
-  });
-
-  context('with non-number arguments', () => {
-    it('should throw error', () => {
-      expect(() => {
-        sum(1, 2, '3', [4], 5);
-      }).to.throw(TypeError, 'sum() expects only numbers.');
-    });
+describe('Test Users API', () => {
+  // Test users route
+  describe('POST /users/login: Case Correct Username and Password', () => {
+    it('Should return user info, isAuthenticate and access_token', (done) => {
+      const genericUser = {
+        username: 'an@gmail.com',
+        password: '123456',
+      };
+      chai
+        .request(server)
+        .post('/users/login')
+        .send(genericUser)
+        .then((response) => {
+          response.should.have.status(200);
+          response.body.should.be.a('object');
+          console.log('Pass body type!');
+          response.body.should.have.property('isAuthenticated').eq(true);
+          console.log('Pass field isAuthenticated!');
+          response.body.should.have.property('user').to.deep.equal({
+            email: 'an@gmail.com',
+            role: 1,
+            fullName: "User's Name",
+          });
+          console.log('Pass field user!');
+          done();
+        })
+        .catch(done);
+    }).timeout(4000);
+    // Should have timeout because takes time to connect to cloud database!
   });
 });
