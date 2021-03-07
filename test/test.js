00a4d5880e0c0e29d03f9e0efad0802af40385c7
Linkin Ben <linkinben@localhost.localdomain>
2021-03-07 11:32:21 +0700

    Separate Test

--- test/test.js
+++ test/test.js
@@ -1,67 +1,17 @@
-const chai = require('chai');
-const request = require('chai-http');
-
-chai.should();
-chai.use(request);
-
-const server = require('../app').server;
-chai.request.agent(server);
+function importTest(name, path) {
+  describe(name, function () {
+    require(path);
+  });
+}
 
 describe('Test Users API', () => {
-  // Test users route
-  describe('POST /users/login:', () => {
-    // Best Case
-    it('Case Correct Username and Password', (done) => {
-      const genericUser = {
-        username: 'an@gmail.com',
-        password: '123456',
-      };
-      chai
-        .request(server)
-        .post('/users/login')
-        .send(genericUser)
-        .then((response) => {
-          response.should.have.status(200);
-          response.body.should.be.a('object');
-          console.log('Pass body type!');
-          response.body.should.have.property('isAuthenticated').eq(true);
-          console.log('Pass field isAuthenticated!');
-          response.body.should.have.property('user').to.deep.equal({
-            email: 'an@gmail.com',
-            role: 1,
-            fullName: "User's Name",
-          });
-          console.log('Pass field user!');
-          done();
-        })
-        .catch(done);
-    });
-    // Should have timeout because takes time to connect to cloud database!
+  beforeEach(function () {
+    console.log('Try running something before each test!');
+  });
 
-    // Case wrong username or password
-    it('Case Wrong Username or Password', (done) => {
-      const genericUser = {
-        username: 'an@gmail.com',
-        password: '12345',
-      };
-      chai
-        .request(server)
-        .post('/users/login')
-        .send(genericUser)
-        .then((response) => {
-          response.should.have.status(400);
-          console.log('Pass status code!');
-          response.body.should.be.a('object');
-          console.log('Pass body type!');
-          response.body.should.have
-            .property('msgBody')
-            .eq('Password not match!');
-          console.log('Pass msgBody!');
-          response.body.should.have.property('msgError').eq(true);
-          console.log('Pass msgError!');
-          done();
-        })
-        .catch(done);
-    });
+  importTest('users.test', './users/users.test');
+
+  after(function () {
+    console.log('Run all test!');
   });
 });
